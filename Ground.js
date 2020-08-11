4d9653627eb2974a1e51526f3c2cb407f8139213
Sanjana <jainmansan@gmail.com>
2020-08-11 14:34:07 +0530

    changed Ground.js

--- Ground.js
+++ Ground.js
@@ -12,6 +12,8 @@ class Ground {
       var pos =this.body.position;
       rectMode(CENTER);
       fill("brown");
+      strokeWeight(4);
+      stroke("green");
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
